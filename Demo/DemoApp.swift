cc6c14cb3accc19747a5f7524e1dc415c1f493e4
durukanoktay <durukanoktay81@gmail.com>
2023-12-03 13:59:44 +0300

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Durukan Oktay on 3.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
