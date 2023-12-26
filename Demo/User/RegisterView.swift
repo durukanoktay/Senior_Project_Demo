4a2d42d6e442f4fc59098fa850f1450c81f942cd
durukanoktay <durukanoktay81@gmail.com>
2023-12-26 15:19:05 +0300

    LoginView and RegisterView: Buttons added

--- Demo/User/RegisterView.swift
+++ Demo/User/RegisterView.swift
@@ -2,73 +2,17 @@
 //  RegisterView.swift
 //  Demo
 //
-//  Created by Durukan Oktay on 22.12.2023.
+//  Created by Durukan Oktay on 26.12.2023.
 //
 
 import SwiftUI
 
-
-
 struct RegisterView: View {
-    @StateObject var RegisterViewModel: RegisterViewModel
     var body: some View {
-        
-        
-        
-        NavigationView {
-            VStack {
-                Text("Hesap Oluştur")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                
-                VStack(alignment: .leading) {
-                    Text("Ad")
-                        .font(.headline)
-                    TextField("Adınız", text: $RegisterViewModel.name)
-                        .padding()
-                        .background(Color(.systemGray6))
-                        .cornerRadius(10)
-                        .padding(.bottom, 20)
-                    
-                    Text("Soyad")
-                        .font(.headline)
-                    TextField("Soyadınız", text: $RegisterViewModel.surname)
-                        .padding()
-                        .background(Color(.systemGray6))
-                        .cornerRadius(10)
-                        .padding(.bottom, 20)
-                    
-                    Text("E-posta")
-                        .font(.headline)
-                    TextField("E-posta adresiniz", text: $RegisterViewModel.email)
-                        .padding()
-                        .background(Color(.systemGray6))
-                        .cornerRadius(10)
-                        .padding(.bottom, 20)
-                        .textInputAutocapitalization(.never)
-                        .keyboardType(.emailAddress)
-                    
-                    Text("Şifre")
-                        .font(.headline)
-                    SecureField("Şifreniz", text: $RegisterViewModel.password)
-                        .padding()
-                        .background(Color(.systemGray6))
-                        .cornerRadius(10)
-                        .padding(.bottom, 20)
-                    
-                  
-                }
-                .padding()
-            }
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-
-
-
 #Preview {
-
     RegisterView()
-    
 }
