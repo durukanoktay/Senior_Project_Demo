a3ac04699dd6eb89fb880b0cbbc2ed4a34428483
durukanoktay <durukanoktay81@gmail.com>
2024-01-17 16:15:42 +0300

    chatManager send message section

--- Managers/ChatManager.swift
+++ Managers/ChatManager.swift
@@ -6,24 +6,38 @@
 //
 
 import SwiftUI
-
 struct ChatManager: View {
     
+    @State private var newMessage: String = ""
+    
     var chat: [ChatData] = [
-        ChatData( content: "Merhaba!", sender: true ),
-        ChatData( content: "Ne zaman Müsaitsin?", sender: true ),
-        ChatData( content: "Her zaman müsaitim", sender: false ),
-        ChatData( content: "100.Yılda buluşalım istersen?", sender: true ),
-        ChatData( content: "Tamam, 11.00 gibi boş musun?", sender: false )
-      
+        ChatData(content: "Merhaba!", sender: true),
+        ChatData(content: "Ne zaman Müsaitsin?", sender: true),
+        ChatData(content: "Her zaman müsaitim", sender: false),
+        ChatData(content: "100.Yılda buluşalım istersen?", sender: true),
+        ChatData(content: "Tamam, 11.00 gibi boş musun?", sender: false)
     ]
 
     
     var body: some View {
-        List {
-            ForEach(chat, id: \.content) { message in
-                MessageView(content: message.content, isSender: message.sender)
+        VStack {
+            List {
+                ForEach(chat, id: \.content) { message in
+                    MessageView(content: message.content, isSender: message.sender)
+                }
             }
+
+            HStack {
+                TextField("Type a message...", text: $newMessage)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+
+                Button("Send") {
+                    
+                  //  (ChatData(content: newMessage, sender: true))
+                    newMessage = "" // Clear the text field
+                }
+            }
+            .padding()
         }
     }
 }
@@ -31,7 +45,7 @@ struct ChatManager: View {
 struct MessageView: View {
     var content: String
     var isSender: Bool
-
+    
     var body: some View {
         HStack {
             if isSender {
